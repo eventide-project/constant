981845ee792c710e9bed1c7085bb8d75c3597c69
Scott Bellware <sbellware@gmail.com>
2026-03-06 09:52:50 -0800

    Optional constant wherein a new constant is defined is removed

--- lib/constant/define.rb
+++ lib/constant/define.rb
@@ -1,8 +1,7 @@
 module Constant
   module Define
-    def self.call(constant_name, receiver_constant, constant=nil)
-      constant ||= Module.new
-
+    def self.call(constant_name, receiver_constant)
+      constant = Module.new
       receiver_constant.const_set(constant_name, constant)
     end
   end
