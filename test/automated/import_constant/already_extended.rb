8cd95a8d84470f0ab3fd4682afdf0ef76e640a8a
Scott Bellware <sbellware@gmail.com>
2026-03-27 08:22:14 -0700

    Test name is corrected (import rather than extend)

--- test/automated/import_constant/already_extended.rb
+++ test/automated/import_constant/already_extended.rb
@@ -1,7 +1,7 @@
 require_relative "../automated_init"
 
 context "Import Constant" do
-  context "Already Extended" do
+  context "Already Imported" do
     source_constant = Controls::Constant.example(name: "Source") do
       const :SomeInnerConstant
     end
