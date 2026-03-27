6ce1bd40e0b2816758725fb7589459df7ba9de5e
Scott Bellware <sbellware@gmail.com>
2026-03-27 08:41:57 -0700

    Test structure is clarified

---
+++ test/automated/import_constant/already_imported/already_imported.rb
@@ -0,0 +1,22 @@
+require_relative "../../automated_init"
+
+context "Import Constant" do
+  context "Already Imported" do
+    source_constant = Controls::Constant.example(name: "Source") do
+      const :SomeInnerConstant
+    end
+
+    receiver_constant = Controls::Constant.example(name: "Receiver")
+    receiver_constant.include(source_constant)
+
+    comment "Source Constant: #{source_constant.inspect}"
+    comment "Receiver Constant: #{receiver_constant.inspect}"
+    comment "Receiver Ancestors: #{receiver_constant.ancestors.inspect}"
+
+    test "Raises error" do
+      assert_raises(Constant::Import::Error) do
+        Constant::Import.(source_constant, receiver_constant)
+      end
+    end
+  end
+end
