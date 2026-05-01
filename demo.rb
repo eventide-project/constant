364095993cef3987ef8a75e8b8fddbc756c3fe52
Scott Bellware <sbellware@gmail.com>
2026-05-01 09:40:30 -0700

    Demo includes examples of the macro and the API

--- demo.rb
+++ demo.rb
@@ -17,11 +17,26 @@ pp "Ancestors: #{SomeModule.ancestors}"
 pp "Inner Constants: #{SomeModule.constants}"
 pp "some_method Defined: #{SomeModule.method_defined?(:some_method)}"
 
+
+# Macro
 module SomeOtherModule
-  Constant::Import.(Extension, self)
+  include Constant::Import
+
+  import Extension
 end
 
 pp "SomeOtherModule Imports Extension"
 pp "Ancestors: #{SomeOtherModule.ancestors}"
 pp "Inner Constants: #{SomeOtherModule.constants}"
 pp "some_method Defined: #{SomeOtherModule.method_defined?(:some_method)}"
+
+
+# API
+module YetAnotherModule
+  Constant::Import.(Extension, self)
+end
+
+pp "YetAnotherModule Imports Extension"
+pp "Ancestors: #{YetAnotherModule.ancestors}"
+pp "Inner Constants: #{YetAnotherModule.constants}"
+pp "some_method Defined: #{YetAnotherModule.method_defined?(:some_method)}"
