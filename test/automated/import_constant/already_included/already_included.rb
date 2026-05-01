a3e3ef269d66049a51716610616ba798177ae7ca
Scott Bellware <sbellware@gmail.com>
2026-05-01 11:07:41 -0700

    Already included, rather than already imported

--- test/automated/import_constant/already_included/already_included.rb
+++ test/automated/import_constant/already_included/already_included.rb
@@ -1,18 +1,20 @@
 require_relative "../../automated_init"
 
 context "Import Constant" do
-  context "Already Imported" do
+  context "Already Included" do
     source_constant = Controls::Constant.example(
       name: "Source",
       inner_constants: %i(SomeInnerConstant)
     )
 
     receiver_constant = Controls::Constant.example(name: "Receiver")
+
     receiver_constant.include(source_constant)
 
     comment "Source Constant: #{source_constant.inspect}"
     comment "Receiver Constant: #{receiver_constant.inspect}"
     comment "Receiver Ancestors: #{receiver_constant.ancestors.inspect}"
+    comment "Receiver Constants: #{receiver_constant.constants(false).inspect}"
 
     test "Raises error" do
       assert_raises(Constant::Import::Error) do
