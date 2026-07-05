06bff2fe00ed7fc3ac3ae16109ac037529297940
Scott Bellware <sbellware@gmail.com>
2026-07-05 11:20:50 -0700

    Comment is corrected

--- proof.rb
+++ proof.rb
@@ -16,7 +16,7 @@ assert SomeReceiver.const_defined?(:SomeModule)
 assert SomeReceiver.const_defined?(:SomeInnerModule)
 refute SomeReceiver.const_defined?(:YetAnotherInnerModule)
 
-# Including a module also causes the module's to be accessible
+# Including a module also causes the module to be accessible
 # via Object's constant lookup path, even though the constant
 # was not explicitly included into Object
 assert Object.const_defined?(:SomeModule)
