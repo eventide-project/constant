6f5142eb5f7ab64ca1facfb87e0a7dd1a53508e2
Scott Bellware <sbellware@gmail.com>
2026-07-07 16:23:27 -0700

    Demo is clarified

--- demo.rb
+++ demo.rb
@@ -1,47 +1,70 @@
 require_relative "init"
 
-module Extension
-  def some_method
-  end
-
-  module InnerModule
-  end
-end
-
-module SomeModule
-  include Extension
-end
-
 puts "Via Include"
-puts "> SomeModule include Extension"
+puts "- - -"
+puts
+
+code = <<~CODE
+  module Extension
+    def some_method
+    end
+
+    module InnerModule
+    end
+  end
+
+  module SomeModule
+    include Extension
+  end
+CODE
+
+eval code
+
+puts code
+puts
+
 puts "SomeModule Ancestors: #{SomeModule.ancestors}"
 puts "SomeModule Inner Constants: #{SomeModule.constants}"
 puts "SomeModule some_method Defined: #{SomeModule.method_defined?(:some_method)}"
 
-
-# Macro
-module SomeOtherModule
-  include Constant::Import
-
-  import Extension
-end
-
 puts
-puts "Via import Macro"
-puts "> SomeOtherModule import Extension"
+puts "Via Macro"
+puts "- - -"
+puts
+
+code = <<~CODE
+  module SomeOtherModule
+    include Constant::Import
+
+    import Extension
+  end
+CODE
+
+eval code
+
+puts code
+puts
+
 puts "SomeOtherModule Ancestors: #{SomeOtherModule.ancestors}"
 puts "SomeOtherModule Inner Constants: #{SomeOtherModule.constants}"
 puts "SomeOtherModule some_method Defined: #{SomeOtherModule.method_defined?(:some_method)}"
 
-
-# API
-module YetAnotherModule
-  Constant::Import.(Extension, self)
-end
-
 puts
-puts "Via Constant::Import API"
-puts "> YetAnotherModule Constant::Import.(Extension, self)"
+puts "Via API"
+puts "- - -"
+puts
+
+code = <<~CODE
+  module YetAnotherModule
+    Constant::Import.(Extension, self)
+  end
+CODE
+
+eval code
+
+puts code
+puts
+
 puts "YetAnotherModule Ancestors: #{YetAnotherModule.ancestors}"
 puts "YetAnotherModule Inner Constants: #{YetAnotherModule.constants}"
 puts "YetAnotherModule some_method Defined: #{YetAnotherModule.method_defined?(:some_method)}"
